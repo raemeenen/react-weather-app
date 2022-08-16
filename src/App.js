4950353ce877d4c367aa353f58111f3d149feeec
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-08-15 21:31:35 -0500

    added footer with link to GitHub

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        <a href="https://github.com/raemeenen/react-weather-app" target="_blank">Open-source code</a> by <a href="https://visionary-lily-770552.netlify.app/" target="_blank">Rachel Meenen</a>
+      </footer>
     </div>
   );
 }
