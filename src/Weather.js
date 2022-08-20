034e3a18ed39a7ca395aa15debe7e85dadca782b
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-08-20 11:00:48 -0500

    adjusted search form

--- src/Weather.js
+++ src/Weather.js
@@ -7,13 +7,14 @@ export default function Weather () {
       <form>
         <div className="row">
           <div className="col-9">
-            <input type="search" placeholder="Search for a city" className="city-search"/>
+            <input type="search" placeholder="Enter a city" className="form-control" />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary"/> 
+            <input type="submit" value="Submit" className="btn btn-primary" />
           </div>
         </div>
       </form>
+      <h1>Kansas City</h1>
       <div className="row">
         <div className="col-6">
           <div>
