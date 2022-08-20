fef8372cc39cf389bcac29300855051fea567514
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-08-20 11:37:59 -0500

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -7,31 +7,29 @@ export default function Weather () {
       <form>
         <div className="row">
           <div className="col-9">
-            <input type="search" placeholder="Enter a city" className="form-control" />
+            <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on" />
           </div>
           <div className="col-3">
-            <input type="submit" value="Submit" className="btn btn-primary" />
+            <input type="submit" value="Submit" className="btn btn-primary w-100" />
           </div>
         </div>
       </form>
       <h1>Kansas City</h1>
+      <div>
+        <ul>
+          <li>Wednesday 8:18</li>
+          <li>Mostly cloudy</li>
+        </ul>
+      </div>
       <div className="row">
         <div className="col-6">
-          <div>
-            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" alt="Clear with periodic clouds"/> 
-            77°C
-          </div>
-          <div>
-            <ul>
-              <li>Humidity: 60%</li>
-              <li>Wind: 0 mph</li>
-            </ul>
-          </div>
+          <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" alt="Clear with periodic clouds"/>
+          <span className="temperature">77</span><span className="unit"> °F</span>
         </div>
-        <div className="col-6">
+        <div  className="col-6">
           <ul>
-            <li>Wednesday 8:18</li>
-            <li>Mostly cloudy</li>
+            <li>Humidity: 60%</li>
+            <li>Wind: 0 mph</li>
           </ul>
         </div>
       </div>
