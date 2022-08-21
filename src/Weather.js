ac51783248910cc8c49df3ef9eaf7b84bfa0d2ee
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-08-21 13:36:10 -0500

    added correct weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather (props) {
       date: new Date(response.data.dt * 1000),
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
