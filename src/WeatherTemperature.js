8f4ce137b54f297580b4f9b3903d9c57abcafe4e
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-08-26 15:12:19 -0500

    added useEffect to update forecast; removed celsius

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,31 +2,10 @@ import React, { useState } from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature (props) {
-  const [unit, setUnit] = useState("fahrenheit");
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function showFahrenheit (event) {
-    event.preventDefault();
-    setUnit("fahrenheit")
-  }
-
-  if (unit === "fahrenheit") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.fahrenheit)}</span>
-        <span className="unit"> °F | <a href="/" onClick={showCelsius}>°C</a></span>
-      </div>
-    );
-  } else {
-    let celsius = (props.fahrenheit - 32) * 5 / 9;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(celsius)}</span>
-        <span className="unit"> <a href="/" onClick={showFahrenheit}>°F</a> | °C</span>
-      </div>
-    )
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.fahrenheit)}</span>
+      <span className="unit"> °F</span>
+    </div>
+  );
 }
