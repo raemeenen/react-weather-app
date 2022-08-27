a5b313a42c3abaadb1d9df7bcd941a3bee57af46
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-08-27 10:17:13 -0500

    added load function to WeatherForecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,6 +17,15 @@ export default function WeatherForecast (props) {
     setLoaded(true);
   }
 
+  function load () {
+    const apiKey = "1a94404a2199216c8565f22d4b4c9832";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -35,12 +44,7 @@ export default function WeatherForecast (props) {
       </div>
     )
   } else {
-    const apiKey = "1a94404a2199216c8565f22d4b4c9832";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`
-
-    axios.get(apiUrl).then(handleResponse);
+    load ()
 
     return null;
   }
